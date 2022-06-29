c8680cdd77a386768e9724d1f159222fca13a6a7
Son Vo <vnsonvo@gmail.com>
2022-06-29 17:34:36 +0700

    finish the sort array by string length 7 kyu challenge

---
+++ 7 kyu/sort_array_by_string_length.js
@@ -0,0 +1,20 @@
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+// sortByLength(["Beg", "Life", "I", "To"]),["I", "To", "Beg", "Life"]
+// sortByLength(["", "Moderately", "Brains", "Pizza"]),["", "Pizza", "Brains", "Moderately"]
+// sortByLength(["Longer", "Longest", "Short"]),["Short", "Longer", "Longest"]
+
+function sortByLength(array) {
+  // Return an array containing the same strings, ordered from shortest to longest
+  return array.sort((stringA, stringB) => stringA.length - stringB.length);
+}
