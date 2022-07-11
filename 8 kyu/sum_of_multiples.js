fe92fe8298531449539e63e315299fd16b9ba16d
Son Vo <vnsonvo@gmail.com>
2022-07-11 17:21:37 +0700

    sum of multiples

---
+++ 8 kyu/sum_of_multiples.js
@@ -0,0 +1,33 @@
+/* Your Job
+Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+Examples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID" */
+
+// PREP
+// Parameter: numbers n and m
+// Return: total (number)
+// Example: 2,9 => 20
+
+// Answer
+function sumMul(n, m) {
+  // n >= m : invalid
+  if (n >= m) return "INVALID";
+  let total = n;
+  let count = 2;
+  // while loop to calculate the sum
+  while (n * count < m) {
+    total += n * count;
+    count++;
+  }
+  return total;
+}
+
+sumMul(0, 0);
+sumMul(2, 9);
