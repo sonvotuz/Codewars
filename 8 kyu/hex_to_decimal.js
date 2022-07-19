48ba1431f4802351a352ea01b533c1663c27540c
Son Vo <vnsonvo@gmail.com>
2022-07-19 20:06:52 +0700

    hex to decimal

---
+++ 8 kyu/hex_to_decimal.js
@@ -0,0 +1,13 @@
+/* Complete the function which converts hex number (given as a string) to a decimal number. */
+
+// PREP
+// Parameter: string
+// Return: number
+// Example: 'a' => 10
+
+// Answer
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
+
+hexToDec("10");
