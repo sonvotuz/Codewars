9cb5e1a3028e1354c24b681a5ae459a269384ff8
Son Vo <vnsonvo@gmail.com>
2022-07-23 21:29:15 +0700

    man in the west

---
+++ 8 kyu/man_in_the_west.js
@@ -0,0 +1,15 @@
+/* A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
+
+So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false. */
+
+// PREP
+// Parameter: array of string
+// Return: boolean
+// Example: ['gold', 'stone'] ==> true
+
+// Answer
+function checkTheBucket(bucket) {
+  return bucket.some((el) => el === "gold");
+}
+
+checkTheBucket(["stone", "stone", "stone", "stone", "stone"]);
