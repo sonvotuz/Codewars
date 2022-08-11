6b0e4ef655c17edc147d3eaad8bc7d11e0ed681b
Son Vo <vnsonvo@gmail.com>
2022-08-11 10:13:49 +0700

    type of sum

---
+++ 8 kyu/type_of_sum.js
@@ -0,0 +1,11 @@
+/* Return the type of the sum of the two arguments */
+
+// PREP
+// Parameter: two parameters
+// Return: type
+// Example: 2, 3 ==> number
+
+// Answer
+function typeOfSum(a, b) {
+  return typeof (a + b)
+}
