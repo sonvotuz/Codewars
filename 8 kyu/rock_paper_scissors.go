e07d7f60700380546c756d71bb9cf20ce25b0b3f
Son Vo <vnsonvo@gmail.com>
2024-01-26 23:38:42 +0700

    rock paper scissors 8kyu in go

---
+++ 8 kyu/rock_paper_scissors.go
@@ -0,0 +1,30 @@
+/*
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+*/
+
+// PREP
+// Parameter: string, string
+// Return: string
+// Example: "scissors", "paper" => "Player 1 won!"
+
+// Answer
+
+package kata
+
+var m = map[string]string{"rock": "paper", "paper": "scissors", "scissors": "rock"}
+
+func Rps(a, b string) string {
+	if a == b {
+		return "Draw!"
+	}
+	if m[a] == b {
+		return "Player 2 won!"
+	}
+	return "Player 1 won!"
+}
